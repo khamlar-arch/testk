4bc26b442c6bb42d392a9bfeec60a304f1e59e93
khamlar-arch <khamlar8527@gmail.com>
2026-08-30 07:55:41 +0700

    Update ResultsScreenState.hx

--- src/funkin/states/ResultsScreenState.hx
+++ src/funkin/states/ResultsScreenState.hx
@@ -12,7 +12,7 @@ import funkin.shaders.TileLine;
 	public var make:Float->FlxSpriteGroup->Float;
 }
 
-class ResultsScreenState extends flixel.FlxSubState {
+class ResultsScreenState extends FunkinSubstate {
 	public static var lastPlay:PlayData;
 	
 	var accepted:Bool = false;
